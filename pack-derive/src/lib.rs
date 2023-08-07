981d4a8bbf010e1f09df55e32d95e6ebb7783531
Kalyan Sriram <kalyan@coderkalyan.com>
2023-08-07 09:49:28 -0700

    add extra data derive macro

---
+++ pack-derive/src/lib.rs
@@ -0,0 +1,55 @@
+extern crate proc_macro;
+
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Data, Fields};
+
+#[proc_macro_derive(ExtraData)]
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+    let expanded = match input.data {
+        Data::Struct(ref data) => {
+            match data.fields {
+                Fields::Named(ref fields) => {
+                    let named0 = fields.named.pairs().map(|x| x.value().ident.clone());
+                    let named1 = named0.clone();
+                    let types = fields.named.pairs().map(|x| x.value().ty.clone());
+
+                    let indices = (0..(named0.len() as u32)).collect::<Vec<_>>();
+
+                    quote! {
+                        impl extra::ExtraData<#name> for #name {
+                            fn pack(&self, vec: &mut extra::Vec) {
+                                #(vec.push(self.#named0.to_extra_data());)*
+                            }
+
+                            fn unpack(slice: extra::Slice, index: extra::Index) -> #name {
+                                #name {
+                                    #(#named1: #types::from(slice[index + #indices].value() as usize)),*
+                                }
+                            }
+                        }
+                    }
+                },
+                Fields::Unnamed(ref _fields) => {
+                    quote!()
+                },
+                Fields::Unit => {
+                    quote!()
+                }
+            }
+        },
+        Data::Enum(_) | Data::Union(_) => unimplemented!(),
+    };
+
+    // panic!("{:?}", expanded);
+    proc_macro::TokenStream::from(expanded)
+
+    // let DeriveInput { ident, data, .. } = parse_macro_input!(input);
+    // let output = quote! {
+    //     impl Pack for #ident {}
+    // };
+    // output.into()
+}
+
---
+++ src/extra.rs
@@ -0,0 +1,60 @@
+#[derive(Clone, Copy)]
+pub struct Data(u32);
+
+impl Data {
+    pub fn from(value: u32) -> Data {
+        Data(value)
+    }
+    pub fn value(self) -> u32 {
+        self.0
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Index(u32);
+
+impl Index {
+    pub fn value(self) -> usize {
+        self.0 as usize
+    }
+
+    pub fn from(index: usize) -> Index {
+        assert!(index <= (u32::MAX as usize));
+        Index(index as u32)
+    }
+
+    pub fn to_extra_data(self) -> Data {
+        Data::from(self.0)
+    }
+}
+
+impl std::ops::Add<u32> for Index {
+    type Output = Index;
+
+    fn add(self, rhs: u32) -> Index {
+        assert!(self.0 <= (u32::MAX - rhs));
+        Index(self.0 + rhs)
+    }
+}
+
+pub type Vec = std::vec::Vec<Data>;
+
+pub struct Slice<'a>(&'a [Data]);
+
+impl <'a> std::ops::Index<Index> for Slice<'a> {
+    type Output = Data;
+
+    fn index(&self, index: Index) -> &'a Data {
+        &self.0[index.value()]
+    }
+}
+
+pub trait ExtraData<T> {
+    fn pack(&self, vec: &mut Vec);
+    fn unpack(slice: Slice, index: Index) -> T;
+}
+
+pub struct Range {
+    pub start: Index,
+    pub end: Index,
+}
